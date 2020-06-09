9db689fe850a65884766455cc84af55c41b2320a
Vilar da Camara Neto <vilarneto@gmail.com>
2020-06-09 15:38:07 -0400

    Initial commit

---
+++ autolayout-2020/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  autolayout-2020
+//
+//  Created by Vilar da Camara Neto on 09/06/20.
+//  Copyright © 2020 Vilar da Camara Neto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
